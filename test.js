2d4e6ce20a8093eb2ab21013c4b7045f4e6db2ac
shinnn <snnskwtnb@gmail.com>
2014-08-28 21:48:22 +0900

    improve test coverage

--- test.js
+++ test.js
@@ -15,6 +15,7 @@ test('http-s', function(t) {
     'should load `https` module when the string has https protocol.',
     'should load `https` module when the object has https property.',
     'should throw an error when the argument is not a valid string or object.',
+    'should throw an error when the string doesn\'t include any protocols.',
     'should throw an error when the string doesn\'t have http or https protocol.',
     'should throw an error when the object doesn\'t have http or https property.'
   ];
@@ -26,8 +27,9 @@ test('http-s', function(t) {
   t.deepEqual(httpOrHttps('https://www.npmjs.org/'), https, behaviors[2]);
   t.deepEqual(httpOrHttps(url.parse('https://github.com/')), https, behaviors[3]);
   t.throws(httpOrHttps.bind(null, 1234), TypeError, behaviors[4]);
-  t.throws(httpOrHttps.bind(null, 'ws://echo.websocket.org'), behaviors[5]);
-  t.throws(httpOrHttps.bind(null, url.parse('git://github.com/npm/npm.git')), behaviors[6]);
+  t.throws(httpOrHttps.bind(null, 'foo'), behaviors[5]);
+  t.throws(httpOrHttps.bind(null, 'ws://echo.websocket.org'), behaviors[6]);
+  t.throws(httpOrHttps.bind(null, url.parse('git://github.com/npm/npm.git')), behaviors[7]);
 
   t.end();
 });
